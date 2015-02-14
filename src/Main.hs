c116868082582f39c508fa76c9773c07706ad10f
Ben James <bmjames@gmail.com>
2015-02-14 10:56:37 +0000

    Option parsing for register & unregister commands

--- src/Main.hs
+++ src/Main.hs
@@ -2,5 +2,24 @@ module Main where
 
 import Config (loadConfig)
 
+import Options.Applicative
+
+data Opts = Register [FilePath]
+          | Unregister [FilePath]
+          deriving (Eq, Ord, Show)
+
+parseOpts :: Parser Opts
+parseOpts = subparser $
+     command "register" (info register (progDesc "Register one or more repositories"))
+  <> command "unregister" (info unregister (progDesc "Unregister one ore more repositories"))
+
+  where
+    register = Register <$> paths
+    unregister = Unregister <$> paths
+    paths =  many (strArgument (metavar "PATH"))
+
 main :: IO ()
-main = loadConfig >>= print
+main = do
+  opts <- execParser $ info (helper <*> parseOpts) fullDesc
+  print opts
+  loadConfig >>= print
