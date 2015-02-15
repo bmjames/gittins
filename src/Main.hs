28fc690058468692415ab95ca6ad9721521e488e
Ben James <bmjames@gmail.com>
2015-02-15 16:03:41 +0000

    Implement unregister command

--- src/Main.hs
+++ src/Main.hs
@@ -25,18 +25,33 @@ register paths = modifyConfig $ \config -> foldlM addRepo config paths
 
   where
     addRepo :: Config -> FilePath -> IO Config
-    addRepo (Config repos) p = do
+    addRepo config@(Config repos) p = do
       canonical <- canonicalizePath p
       if any (\r -> path r == canonical) repos
         then do putStrLn ("Path [" ++ canonical ++ "] is already registered.")
-                return (Config repos)
+                return config
         else do putStrLn ("Registering [" ++ canonical ++ "]")
                 let repo = Repository canonical
                 return $ Config (repo : repos)
+
+-- | Entry point for unregister command
+unregister :: [FilePath] -> IO ()
+unregister paths = modifyConfig $ \config -> foldlM rmRepo config paths
+
+  where
+    rmRepo :: Config -> FilePath -> IO Config
+    rmRepo config@(Config repos) p = do
+      canonical <- canonicalizePath p
+      if not $ any (\r -> path r == canonical) repos
+         then do putStrLn ("Path [" ++ canonical ++ "] does not appear to be registered.")
+                 return config
+         else do putStrLn ("Unregistering [" ++ canonical ++ "]")
+                 let repos' = filter (\r -> path r /= canonical) repos
+                 return $ Config repos'
 
 main :: IO ()
 main = do
   opts <- execParser $ info (helper <*> parseOpts) fullDesc
   case opts of
     Register paths   -> register paths
-    Unregister paths -> return ()
+    Unregister paths -> unregister paths
