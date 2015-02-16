16120160d622a67f7bc704cbcbb8b68f8c1c3932
Ben James <bmjames@gmail.com>
2015-02-16 21:40:58 +0000

    Implement add-to-group and remove-from-group commands

--- src/Config.hs
+++ src/Config.hs
@@ -2,17 +2,25 @@
 
 module Config (
     Config(..)
+  , GroupId
   , Repository(..)
   , loadConfig
   , saveConfig
   , modifyConfig
+  , modifyRepository
 ) where
 
+import Data.Foldable (foldMap)
 import Data.HashMap.Strict (HashMap, empty, foldrWithKey, fromList)
 import Data.Ini (Ini(..), readIniFile, writeIniFile)
+import Data.List (nub)
+import Data.Maybe (catMaybes)
 import Data.Text (Text, pack, unpack)
 import System.Directory (doesFileExist, getHomeDirectory)
 import System.FilePath ((</>))
+
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
 
 loadConfig :: IO Config
 loadConfig = fmap fromIni loadIni
@@ -24,9 +32,13 @@ saveConfig config = do iniPath <- configFile
 modifyConfig :: (Config -> IO Config) -> IO ()
 modifyConfig f = loadConfig >>= f >>= saveConfig
 
-data Config = Config { repositories :: [Repository] } deriving (Eq, Ord, Show)
+data Config = Config { repositories :: [Repository] }
+            deriving (Eq, Ord, Show)
 
-data Repository = Repository { path :: FilePath } deriving (Eq, Ord, Show)
+type GroupId = Text
+
+data Repository = Repository { path :: FilePath, groups :: [GroupId] }
+                deriving (Eq, Ord, Show)
 
 configFile :: IO FilePath
 configFile = do homeDir <- getHomeDirectory
@@ -49,9 +61,25 @@ fromIni (Ini repos) =
   where
     repository :: Text -> HashMap Text Text -> Repository
     repository path props = Repository (unpack path)
+                                       (foldMap parseGroups $ HM.lookup "group" props)
+
+    parseGroups :: Text -> [GroupId]
+    parseGroups = nub . T.words
 
 toIni :: Config -> Ini
 toIni (Config repos) = Ini $ fromList (map fromRepository repos)
 
   where
-    fromRepository (Repository path) = (pack path, empty)
+    fromRepository :: Repository -> (Text, HashMap Text Text)
+    fromRepository repo@(Repository p gs) = (pack p, settings repo)
+
+    settings :: Repository -> HashMap Text Text
+    settings (Repository _ gs) =
+      let group = if null gs then Nothing else Just ("group", T.unwords $ nub gs)
+      in fromList (catMaybes [group])
+
+modifyRepository :: (Repository -> Maybe Repository) -> Config -> Maybe Config
+modifyRepository f (Config repos) = fmap Config (go repos) where
+  go (r:rs) | Just r' <- f r = Just (r':rs)
+            | otherwise = fmap (r:) (go rs)
+  go [] = Nothing
--- src/Main.hs
+++ src/Main.hs
@@ -2,22 +2,35 @@ module Main where
 
 import Config
 import Data.Foldable (foldlM)
+import Data.List (delete)
+import Data.Text (pack)
 import Options.Applicative
 import System.Directory (canonicalizePath)
 
 data Opts = Register [FilePath]
           | Unregister [FilePath]
+          | AddToGroup GroupId [FilePath]
+          | RemoveFromGroup GroupId [FilePath]
           deriving (Eq, Ord, Show)
 
 parseOpts :: Parser Opts
 parseOpts = subparser $
-     command "register" (info registerOpts (progDesc "Register one or more repositories"))
-  <> command "unregister" (info unregisterOpts (progDesc "Unregister one ore more repositories"))
+     command "register" (info registerOpts
+                              (progDesc "Register one or more repositories"))
+  <> command "unregister" (info unregisterOpts
+                                (progDesc "Unregister one ore more repositories"))
+  <> command "add-to-group" (info addToGroupOpts
+                                  (progDesc "Add one ore more repositories to a group"))
+  <> command "remove-from-group" (info removeFromGroupOpts
+                                       (progDesc "Remove one or more repositories from a group"))
 
   where
     registerOpts = Register <$> paths
     unregisterOpts = Unregister <$> paths
-    paths =  many (strArgument (metavar "PATH"))
+    addToGroupOpts = AddToGroup <$> groupId <*> paths
+    removeFromGroupOpts = RemoveFromGroup <$> groupId <*> paths
+    groupId = pack <$> strArgument (metavar "GROUP")
+    paths = some (strArgument (metavar "PATH"))
 
 -- | Entry point for register command
 register :: [FilePath] -> IO ()
@@ -31,7 +44,7 @@ register paths = modifyConfig $ \config -> foldlM addRepo config paths
         then do putStrLn ("Path [" ++ canonical ++ "] is already registered.")
                 return config
         else do putStrLn ("Registering [" ++ canonical ++ "]")
-                let repo = Repository canonical
+                let repo = Repository canonical []
                 return $ Config (repo : repos)
 
 -- | Entry point for unregister command
@@ -49,9 +62,41 @@ unregister paths = modifyConfig $ \config -> foldlM rmRepo config paths
                  let repos' = filter (\r -> path r /= canonical) repos
                  return $ Config repos'
 
+-- | Entry point for add-to-group command
+addToGroup :: GroupId -> [FilePath] -> IO ()
+addToGroup groupId paths = modifyConfig $ \config -> foldlM addGroup config paths
+
+  where
+    addGroup :: Config -> FilePath -> IO Config
+    addGroup config path = do
+      canonical <- canonicalizePath path
+      let config' = modifyRepository (\(Repository p gs) ->
+            if p == canonical then Just $ Repository p (groupId:gs) else Nothing) config
+      case config' of
+        Just c  -> return c
+        Nothing -> do putStrLn ("Path [" ++ canonical ++ "] does not appear to be registered.")
+                      return config
+
+-- | Entry point for remove-from-group command
+removeFromGroup :: GroupId -> [FilePath] -> IO ()
+removeFromGroup groupId paths = modifyConfig $ \config -> foldlM removeGroup config paths
+
+  where
+    removeGroup :: Config -> FilePath -> IO Config
+    removeGroup config path = do
+      canonical <- canonicalizePath path
+      let config' = modifyRepository (\(Repository p gs) ->
+            if p == canonical then Just $ Repository p (delete groupId gs) else Nothing) config
+      case config' of
+        Just c  -> return c
+        Nothing -> do putStrLn ("Path [" ++ canonical ++ "] does not appear to be registered.")
+                      return config
+
 main :: IO ()
 main = do
   opts <- execParser $ info (helper <*> parseOpts) fullDesc
   case opts of
-    Register paths   -> register paths
-    Unregister paths -> unregister paths
+    Register paths                -> register paths
+    Unregister paths              -> unregister paths
+    AddToGroup groupId paths      -> addToGroup groupId paths
+    RemoveFromGroup groupId paths -> removeFromGroup groupId paths
