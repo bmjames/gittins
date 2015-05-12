c42e01314fe9ed5dd7d753f2766473ecdc605c41
Ben James <bmjames@gmail.com>
2015-05-12 17:59:10 +0000

    Update shell.nix with ghc-mod

--- shell.nix
+++ shell.nix
@@ -1,24 +1,10 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, abstract-par, ansi-wl-pprint, base, directory
-             , filepath, free, hspec, ini, monad-control, monad-par, mtl
-             , optparse-applicative, process, stdenv, text, transformers
-             , transformers-base, unordered-containers
-             }:
-             mkDerivation {
-               pname = "gittins";
-               version = "0.1.0.0";
-               src = ./.;
-               isLibrary = true;
-               isExecutable = true;
-               buildDepends = [
-                 abstract-par ansi-wl-pprint base directory filepath free ini
-                 monad-control monad-par mtl optparse-applicative process text
-                 transformers transformers-base unordered-containers
-               ];
-               testDepends = [ base free hspec ];
-               description = "Tool for managing multiple git repositories";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  override = pkgs.stdenv.lib.overrideDerivation;
 in
-  pkg.env
+  override (import ./shell.generated.nix) (old: {
+    buildInputs = pkgs.haskellngPackages.ghcWithPackages (self:
+      [ self.ghc-mod ] ++ old.buildInputs
+    );
+  })
